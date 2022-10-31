8fdbc9d39a24fe89bd74eda04e003ddbcc8c1241
Mayall <mayall0513@outlook.com>
2022-10-31 13:54:40 +0000

    modified:   data/Database.js

--- data/Database.js
+++ data/Database.js
@@ -4,7 +4,8 @@ const databasePool = new pg.Pool({
   port: parseInt(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE
+  database: process.env.DB_DATABASE,
+  ssl : { rejectUnauthorized: false }
 });
 
 export default databasePool;
